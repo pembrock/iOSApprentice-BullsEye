97163ec402b0b16470b68c36cb40f586e4e2b2d7
Kirill Pasikuta <pembrock@gmail.com>
2019-11-29 18:13:35 +0300

    add IBOuttlet for slider

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,22 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var currentValue: Int = 50
+    
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        currentValue = lroundf(slider.value)
     }
 
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hello, World", message: "This is my first app", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        let message = "The value of the slider is: \(currentValue)"
+        let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
-        print("The value of the slider is now: \(slider.value)")
+        currentValue = lroundf(slider.value)
     }
 }
 
